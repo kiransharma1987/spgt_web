4cefaccbbf80ad8765493178185ab5b0ae8c1b45
Kiran H K <kiransharma1987@gmail.com>
2024-10-04 19:37:36 +0530

    gallery plugin added

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,6 +1,5 @@
-// src/components/Gallery.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Modal, ModalGateway } from 'react-images';
 import image3 from '../gallery/3.jpg';
 import image4 from '../gallery/4.jpg';
 import image5 from '../gallery/5.jpg';
@@ -10,37 +9,54 @@ import image8 from '../gallery/8.jpg';
 import image9 from '../gallery/9.jpg';
 
 const Gallery = () => {
-    // Sample image data
-    const images = [
-        { id: 3, src: image3, alt: 'Event 3', title: 'Event Title 3' },
-        { id: 4, src: image4, alt: 'Event 4', title: 'Event Title 4' },
-        { id: 5, src: image5, alt: 'Event 5', title: 'Event Title 5' },
-        { id: 6, src: image6, alt: 'Event 6', title: 'Event Title 6' },
-        { id: 7, src: image7, alt: 'Event 7', title: 'Event Title 7' },
-        { id: 8, src: image8, alt: 'Event 8', title: 'Event Title 8' },
-        { id: 9, src: image9, alt: 'Event 9', title: 'Event Title 9' },
-        // Add more images as necessary
-    ];
+  const [lightboxIsOpen, setLightboxIsOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(0);
 
-    return (
-        <div className="gallery">
-            <header>
-                <h1>Gallery</h1>
-                <p>Explore moments captured at our temple events.</p>
-            </header>
+  const images = [
+    { src: image3 },
+    { src: image4 },
+    { src: image5 },
+    { src: image6 },
+    { src: image7 },
+    { src: image8 },
+    { src: image9 }
+  ];
 
-            <div className="gallery-grid">
-                {images.map(image => (
-                    <div key={image.id} className="gallery-item">
-                        <img src={image.src} alt={image.alt} />
-                        <h3>{image.title}</h3>
-                    </div>
-                ))}
-            </div>
+  const openLightbox = (index) => {
+    setCurrentImage(index);
+    setLightboxIsOpen(true);
+  };
 
-            <Link to="/" className="btn">Back to Home</Link>
-        </div>
-    );
+  const closeLightbox = () => {
+    setLightboxIsOpen(false);
+  };
+
+  return (
+    <div>
+      <div className="gallery">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image.src}
+            alt={`Gallery Image ${index + 1}`}
+            onClick={() => openLightbox(index)}
+            className="gallery-image"
+          />
+        ))}
+      </div>
+
+      <ModalGateway>
+        {lightboxIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <img
+              src={images[currentImage].src}
+              alt={`Gallery Image ${currentImage + 1}`}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+  );
 };
 
 export default Gallery;
