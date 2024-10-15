a47f237bc132b803f51c4695713fdc021a6e43ea
Kiran H K <kiransharma1987@gmail.com>
2024-10-15 20:54:07 +0530

    changes

--- src/components/home.js
+++ src/components/home.js
@@ -13,8 +13,8 @@ function Home() {
   return (
     <div>
       {/* Hero Section */}
-      <section id="aboutUS" className="hero container">
-        <div className="hero-blue">
+      <section id="aboutUS" classNameName="hero container">
+        <div classNameName="hero-blue">
           <div>
             <h1>
               Sri Prasanna Ganapathi Temple
@@ -24,103 +24,103 @@ function Home() {
               remover of obstacles and harbinger of good fortune. Our temple stands as a beacon of hope,
               spirituality, and community service, welcoming devotees from all walks of life.
             </p>
-            <div className="call-to-action">
-              <a href="#" className="button black">
+            <div classNameName="call-to-action">
+              <a href="#" classNameName="button black">
                 Donate Now
               </a>
-              <a href="#" className="button white">
+              <a href="#" classNameName="button white">
                 Book Pooja
               </a>
             </div>
           </div>
         </div>
-        <div className="hero-yellow">
+        <div classNameName="hero-yellow">
           <img src={ganeshaHeroImg} alt="Ganesha Hero Image" width="100%" />
         </div>
       </section>
 
       {/* About Temple Section */}
-      <section className="about">
+      <section classNameName="about">
         <h2>About the Temple</h2>
         <p>Founded in 1999, Sri Prasanna Ganapathi Seva Charitable Trust has been a beacon of spiritual enlightenment and cultural preservation for over three decades. Our journey began with a small group of devoted individuals who envisioned a sacred space dedicated to Lord Ganesha. From humble beginnings, we have grown into a vibrant community hub, fostering religious understanding and cultural appreciation.</p>
-        <Link to="/about" className="link">Read More</Link>
+        <Link to="/about" classNameName="link">Read More</Link>
       </section>
 
       {/* Upcoming Events */}
-      <section id="events" class="upcoming-events container">
+      <section id="events" className="upcoming-events container">
         <h2>Upcoming Events</h2>
-        <div class="events-container">
-          <div class="events-wrapper">
-            <div class="events-grid">
-              <div class="event-card">
-                <div class="card-content">
-                  <h3 class="card-title" title="Akshaya Tritiya Special Pooja">Akshaya Tritiya Special
+        <div className="events-container">
+          <div className="events-wrapper">
+            <div className="events-grid">
+              <div className="event-card">
+                <div className="card-content">
+                  <h3 className="card-title" title="Akshaya Tritiya Special Pooja">Akshaya Tritiya Special
                     Pooja</h3>
-                  <div class="card-text">
+                  <div className="card-text">
                     <p>Date: May 15th, 2024</p>
                     <p>Time: 10:00 AM</p>
                   </div>
-                  <a href="/events/akshaya-tritiya" class="card-button">More Details</a>
+                  <a href="/events/akshaya-tritiya" className="card-button">More Details</a>
                 </div>
               </div>
 
-              <div class="event-card">
-                <div class="card-content">
-                  <h3 class="card-title" title="Monthly Sankashti Chaturthi">Monthly Sankashti Chaturthi
+              <div className="event-card">
+                <div className="card-content">
+                  <h3 className="card-title" title="Monthly Sankashti Chaturthi">Monthly Sankashti Chaturthi
                   </h3>
-                  <div class="card-text">
+                  <div className="card-text">
                     <p>Date: June 3rd, 2024</p>
                     <p>Time: 6:00 PM</p>
                   </div>
-                  <a href="/events/sankashti-chaturthi" class="card-button">More Details</a>
+                  <a href="/events/sankashti-chaturthi" className="card-button">More Details</a>
                 </div>
               </div>
 
-              <div class="event-card">
-                <div class="card-content">
-                  <h3 class="card-title" title="Annual Temple Anniversary Celebrations">Annual Temple
+              <div className="event-card">
+                <div className="card-content">
+                  <h3 className="card-title" title="Annual Temple Anniversary Celebrations">Annual Temple
                     Anniversary Celebrations</h3>
-                  <div class="card-text">
+                  <div className="card-text">
                     <p>Date: July 20th, 2024</p>
                     <p>Time: 9:00 AM</p>
                   </div>
-                  <a href="/events/temple-anniversary" class="card-button">More Details</a>
+                  <a href="/events/temple-anniversary" className="card-button">More Details</a>
                 </div>
               </div>
 
-              <div class="event-card">
-                <div class="card-content">
-                  <h3 class="card-title" title="Akshaya Tritiya Special Pooja">Akshaya Tritiya Special
+              <div className="event-card">
+                <div className="card-content">
+                  <h3 className="card-title" title="Akshaya Tritiya Special Pooja">Akshaya Tritiya Special
                     Pooja</h3>
-                  <div class="card-text">
+                  <div className="card-text">
                     <p>Date: May 15th, 2024</p>
                     <p>Time: 10:00 AM</p>
                   </div>
-                  <a href="/events/akshaya-tritiya" class="card-button">More Details</a>
+                  <a href="/events/akshaya-tritiya" className="card-button">More Details</a>
                 </div>
               </div>
 
-              <div class="event-card">
-                <div class="card-content">
-                  <h3 class="card-title" title="Monthly Sankashti Chaturthi">Monthly Sankashti Chaturthi
+              <div className="event-card">
+                <div className="card-content">
+                  <h3 className="card-title" title="Monthly Sankashti Chaturthi">Monthly Sankashti Chaturthi
                   </h3>
-                  <div class="card-text">
+                  <div className="card-text">
                     <p>Date: June 3rd, 2024</p>
                     <p>Time: 6:00 PM</p>
                   </div>
-                  <a href="/events/sankashti-chaturthi" class="card-button">More Details</a>
+                  <a href="/events/sankashti-chaturthi" className="card-button">More Details</a>
                 </div>
               </div>
 
-              <div class="event-card">
-                <div class="card-content">
-                  <h3 class="card-title" title="Annual Temple Anniversary Celebrations">Annual Temple
+              <div className="event-card">
+                <div className="card-content">
+                  <h3 className="card-title" title="Annual Temple Anniversary Celebrations">Annual Temple
                     Anniversary Celebrations</h3>
-                  <div class="card-text">
+                  <div className="card-text">
                     <p>Date: July 20th, 2024</p>
                     <p>Time: 9:00 AM</p>
                   </div>
-                  <a href="/events/temple-anniversary" class="card-button">More Details</a>
+                  <a href="/events/temple-anniversary" className="card-button">More Details</a>
                 </div>
               </div>
             </div>
@@ -130,105 +130,105 @@ function Home() {
       </section>
 
       {/* Pooja Services Section */}
-      <section id="poojas" class="pooja-services container">
+      <section id="poojas" className="pooja-services container">
             <h2>Our Pooja Services</h2>
             <p>We offer a range of pooja services to suit your spiritual needs. Choose from our list of services to seek
                 blessings for you and your family.</p>
-            <div class="pooja-grid">
-                <div class="pooja-card">
-                    <div class="card-content">
-                        <h3 class="card-title">Ganesh Homa</h3>
-                        <p class="card-text">This pooja invokes the blessings of Lord Ganesha to remove obstacles and
+            <div className="pooja-grid">
+                <div className="pooja-card">
+                    <div className="card-content">
+                        <h3 className="card-title">Ganesh Homa</h3>
+                        <p className="card-text">This pooja invokes the blessings of Lord Ganesha to remove obstacles and
                             ensure success in all endeavors.</p>
-                        <a href="/poojas/ganesh-homa" class="card-button">Book Now</a>
+                        <a href="/poojas/ganesh-homa" className="card-button">Book Now</a>
                     </div>
                 </div>
 
-                <div class="pooja-card">
-                    <div class="card-content">
-                        <h3 class="card-title">Abhisheka</h3>
-                        <p class="card-text">Performing abhisheka to Lord Ganesha is considered highly auspicious and
+                <div className="pooja-card">
+                    <div className="card-content">
+                        <h3 className="card-title">Abhisheka</h3>
+                        <p className="card-text">Performing abhisheka to Lord Ganesha is considered highly auspicious and
                             brings peace and prosperity to the family.</p>
-                        <a href="/poojas/abhisheka" class="card-button">Book Now</a>
+                        <a href="/poojas/abhisheka" className="card-button">Book Now</a>
                     </div>
                 </div>
 
-                <div class="pooja-card">
-                    <div class="card-content">
-                        <h3 class="card-title">Navagraha Pooja</h3>
-                        <p class="card-text">This pooja is performed to appease the nine celestial bodies and receive
+                <div className="pooja-card">
+                    <div className="card-content">
+                        <h3 className="card-title">Navagraha Pooja</h3>
+                        <p className="card-text">This pooja is performed to appease the nine celestial bodies and receive
                             their blessings for good fortune.</p>
-                        <a href="/poojas/navagraha-pooja" class="card-button">Book Now</a>
+                        <a href="/poojas/navagraha-pooja" className="card-button">Book Now</a>
                     </div>
                 </div>
 
-                <div class="pooja-card">
-                    <div class="card-content">
-                        <h3 class="card-title">Satyanarayana Pooja</h3>
-                        <p class="card-text">Offering prayers to Lord Vishnu, this pooja is performed for prosperity,
+                <div className="pooja-card">
+                    <div className="card-content">
+                        <h3 className="card-title">Satyanarayana Pooja</h3>
+                        <p className="card-text">Offering prayers to Lord Vishnu, this pooja is performed for prosperity,
                             health, and family harmony.</p>
-                        <a href="/poojas/satyanarayana-pooja" class="card-button">Book Now</a>
+                        <a href="/poojas/satyanarayana-pooja" className="card-button">Book Now</a>
                     </div>
                 </div>
             </div>
-            <Link to="/poojas" className="btn">View All Poojas</Link>
+            <Link to="/poojas" classNameName="btn">View All Poojas</Link>
         </section>
 
       {/* Donate Section */}
 
-      <section id="donations" class="donations-section container">
+      <section id="donations" className="donations-section container">
 
             <h2>Support the Temple</h2>
             <p>Your contributions help us preserve the temple's sanctity and carry forward our spiritual and charitable
                 activities.</p>
 
-            <div class="donation-options">
-                <div class="donation-card">
+            <div className="donation-options">
+                <div className="donation-card">
                     <h3>General Donation</h3>
                     <p>Help us with daily temple operations and maintenance. Every bit makes a difference.</p>
-                    <a href="/donate/general" class="btn">Donate Now</a>
+                    <a href="/donate/general" className="btn">Donate Now</a>
                 </div>
 
-                <div class="donation-card">
+                <div className="donation-card">
                     <h3>Special Pooja Donation</h3>
                     <p>Support specific rituals like Ganesh Homa, Abhisheka, and others to seek divine blessings.</p>
-                    <a href="/donate/pooja" class="btn">Donate Now</a>
+                    <a href="/donate/pooja" className="btn">Donate Now</a>
                 </div>
             </div>
 
-            <div class="cta">
-                <p>Want to learn more about how your donations are used? <Link to="/donations" className="btn">Donate Now</Link>.
+            <div className="cta">
+                <p>Want to learn more about how your donations are used? <Link to="/donations" classNameName="btn">Donate Now</Link>.
                 </p>
             </div>
             
         </section>
 
       {/* Gallery Preview Section */}
-      <section id="gallery" class="bento container">
+      <section id="gallery" className="bento container">
             <h2>
                 Gallery
             </h2>
-            <div class="bento-grid">
-                <a href="#" class="bento-item">
+            <div className="bento-grid">
+                <a href="#" className="bento-item">
                     <img src={galImg1} alt="BGCCI" width="100%" />
                 </a>
-                <a href="#" class="bento-item">
+                <a href="#" className="bento-item">
                     <img src={galImg2} alt="Churhview" width="100%" />
                 </a>
-                <a href="#" class="bento-item">
+                <a href="#" className="bento-item">
                     <img src={galImg3} alt="Harley" width="100%" />
                 </a>
-                <a href="#" class="bento-item">
+                <a href="#" className="bento-item">
                     <img src={galImg4} alt="Bunbury" width="100%" />
                 </a>
-                <a href="#" class="bento-item">
+                <a href="#" className="bento-item">
                     <img src={galImg5} alt="Running" width="100%" />
                 </a>
-                <a href="#" class="bento-item">
+                <a href="#" className="bento-item">
                     <img src={galImg6} alt="School" width="100%" />
                 </a>
             </div>
-            <Link to="/gallery" className="link">View Gallery</Link>
+            <Link to="/gallery" classNameName="link">View Gallery</Link>
         </section>
     </div>
   );
